8007e0bd3d38ec5b22589e8af01ef289d062dfdf
Gabriel Vieira <gv1999mpc@hotmail.com>
2021-03-18 22:50:15 +0000

    Comentários no programa completos (exceção - modos)
    Os modos são para ir completando à medida que se é feito o projeto

--- wordStats.sh
+++ wordStats.sh
@@ -4,35 +4,31 @@
 # ───────────────────────────────────────────────────────── GLOBAL VARIABLES ─────
 #
 
-# Input
+# Input variables
 MODE=$1
 FILE=$2
 ISO=$3
 
-# StopWords file
+# StopWords files
 # Example pt.stop_words.txt
-STOP_WORD_FILE="stop_words.txt"
+STOP_WORDS_FILE="stop_words.txt"
 LANG_PATH="./lang"
 
-# Words variables
-declare -A WORDS
-declare -A SORTED_WORDS
-WORDS_LENGHT=0
-
-# Stopwords related
+# Stopwords related variables
 WORD_STATS_TOP=10
-STOP_WORDS_STATUS=false
-STOP_WORDS=()
+
+# Default preview lenght
+PREVIEW_LENGHT=10
 
 # Output file
 OUTPUT_FILE="results/result---"
 OUTPUT_FILE_FORMAT="txt"
 OUTPUT_TEMP=".temp"
 
-# Allowed ISO format
+# Allowed ISOs
 ISOS=("pt" "en")
 
-# Allowed modes
+# Allowed output modes
 MODES=("c" "C" "t" "T" "p" "P")
 
 # Allowed file types
@@ -44,44 +40,75 @@ FILE_TYPES["pdf"]="PDF"
 # ──────────────────────────────────────────────────────────────── FUNCTIONS ─────
 #
 
+# Exits the current execution with a message
+# USAGE: close
 close() {
     echo
-    echo "Done..."
+    echo "CLosing..."
     exit 0
 }
 
-# if in_array {parameter} in {array}; then ... fi
+# Verifies if a value exists in the array
+# USAGE: if in_array {parameter} in {array}; then ... fi
 in_array() {
     if [ "$2" != in ]; then
-        echo "Incorrect usage."
+        echo "!!! Incorrect usage of 'in_array'"
         echo "Correct usage: in_array {key} in {array}"
         return
     fi
     eval '[[ " ${'"$3"'[@]} " =~ " ${'"$1"'} " ]]'
 }
 
-# if index_in_array {index} in {array}; then ... fi
+# Verifies if a index exists in the array
+# USAGE: if index_in_array {index} in {array}; then ... fi
 index_in_array() {
     if [ "$2" != in ]; then
-        echo "Incorrect usage."
+        echo "!!! Incorrect usage of 'index_in_array'"
         echo "Correct usage: in_array {key} in {array}"
         return
     fi
     eval '[ '"$3"'["'"$1"'"] ]'
 }
 
-# Verify if a file exists
+# Verifies if a file exists
+# USAGE: if file_exists FILE_PATH; then ... fi
 file_exists() {
+    if [ "$1" == "" ]; then
+        echo "!!! Incorrect usage of 'file_exists'."
+        echo "A file path must be passed."
+        return
+    fi
     eval '[[ -f "${'"$1"'}" ]]'
 }
 
+# Splits the content of a file in words
+# USAGE: split_words $FILE_PATH
 split_words() {
+    if [ "$1" == "" ]; then
+        echo "!!! Incorrect usage of 'split_words'"
+        echo "A file path must be passed."
+        return
+    fi
     for word in $(cat $1); do
         echo $word
     done
 }
 
+# Prints from a specific file the ammount of lines provided
+# USAGE: print_preview $FILE_PATH {int}
 print_preview() {
+    if [ "$1" == "" ]; then
+        echo "!!! Incorrect usage of 'print_preview'."
+        echo "A file path must be passed as 1st parameter."
+        return
+    fi
+    re='^[0-9]+$'
+    if ! [[ $2 =~ $re ]]; then
+        echo "!!! Incorrect usage of 'print_preview'."
+        echo "A number must be passed as 2nd parameter."
+        return
+    fi
+    unset re
     eval 'head -$2 $1'
     if [ "$(wc -l <"$1")" -gt "$2" ]; then
         printf "\t\t%s" "(...)"
@@ -95,13 +122,10 @@ print_preview() {
 
 clear
 
-#
-# ─────────────────────────────────────────────────────────── VALIDATE INPUT ─────
-#
-
 # Evaluate inputs before proceed
+# In the following code bellow any value not properly assigned causes the program to exit
 
-# Check if mode is permited
+# Check if mode is allowed
 if in_array MODE in MODES; then
     echo "Executing on mode '$MODE'."
 else
@@ -113,49 +137,61 @@ else
     close
 fi
 
-# Check if file exists
+# Check if file exists and the extension is allowed/supported
 if file_exists FILE; then
 
     # get file extension
     extension="${FILE##*.}"
 
     # check if file type is allowed
-    # if [[ ${FILE_TYPES["${extension}"]} ]]; then
     if index_in_array extension in FILE_TYPES; then
         echo "Opening '$FILE' as ${FILE_TYPES[${extension}]} file."
     else
-        echo "File extension '$extension' not allowed."
+        echo "!!! File extension '$extension' not allowed."
         close
     fi
 else
-    echo "File '$FILE' does not exist."
+    echo "!!! Incorrect input file. $FILE"
     close
 fi
 
-# Check if iso is suported, else use default "en"
+# Check if ISO is suported, else use default "en"
 if in_array ISO in ISOS; then
-    echo "ISO will use '$ISO' format."
+    echo "ISO format: '$ISO'."
 else
     ISO="en"
     echo "ISO not defined. Default will be used ('$ISO')."
 fi
 
-STOP_WORD_FILE="$LANG_PATH/$ISO.$STOP_WORD_FILE"
-if file_exists STOP_WORD_FILE; then
-    echo "Stop words file: $STOP_WORD_FILE."
+#
+# If it reaches here means that
+#       ISO is correct and enabled in the ISOS array
+#       File exists and its type is supported
+#
+
+# Sets the STOP_WORDS_FILE path and checks its existance
+# In case it doesn't exist, one is created empty and a warning pops up
+STOP_WORDS_FILE="$LANG_PATH/$ISO.$STOP_WORDS_FILE"
+if file_exists STOP_WORDS_FILE; then
+    echo "Stop words file: $STOP_WORDS_FILE."
 else
-    echo "File not found"
-    #Creates the ISO file
-    # >$STOP_WORD_FILE
+    echo "!!! Stop words file not found ... creating empty"
+    # Creates the ISO file
+    touch $STOP_WORDS_FILE
 fi
 
+# Sets the 'filename' variable the same as input
 filename=$(basename -- "$FILE")
 filename="${filename%.*}"
 
+# Defines global OUTPUT_FILE for easy manipulation
 OUTPUT_FILE="$OUTPUT_FILE$filename.$OUTPUT_FILE_FORMAT"
 
+# Checks if the OUTPUT_FILE exists
+# In case it exists warns that it will be overwritten
+# If it doesn't exist, one will be created
 if file_exists OUTPUT_FILE; then
-    echo "Output file will be overwriten: '$OUTPUT_FILE'"
+    echo "!!! Output file will be overwritten: '$OUTPUT_FILE'"
     true >"$OUTPUT_FILE"
     touch "$OUTPUT_FILE"
 else
@@ -163,17 +199,27 @@ else
     touch "$OUTPUT_FILE"
 fi
 
+#
+# ─────────────────────────────────────────────────────── COMPATIBILITY AREA ─────
+#
+# This area is used to convert different file formats into the default (txt)
+# If this step is well written, then the following code will work without any change
+
+# Converting PDF contents into Text
 if [ "${extension}" == "pdf" ]; then
     touch $OUTPUT_TEMP
     pdftotext $FILE $OUTPUT_TEMP
     FILE=$OUTPUT_TEMP
 fi
 
+#
+# ────────────────────────────────────────────────────────────── BEFORE CODE ─────
+#
+# Here variables that will no longer be used can be deleted
+# Or extra functions can be executed before proceeding
+
 unset filename
 unset extension
-
-# Read stopwords file to an array
-readarray -t STOP_WORDS <$STOP_WORD_FILE
 
 #
 # ───────────────────────────────────────────────────────────────────── CODE ─────
@@ -183,18 +229,23 @@ echo
 case $MODE in
 
 "c")
+    # Stopwords filtered to and presented to a file
+    # Prints PREVIEW_LENGHT to console
     echo "STOPWORDS FILTERED"
-    split_words $FILE | tr -d '.,«»;?' | awk NF | sort | grep -w -v -i -f $STOP_WORD_FILE | uniq -c | sort -rn | cat -n >$OUTPUT_FILE
+    split_words $FILE | tr -d '.,«»;?' | awk NF | sort | grep -w -v -i -f $STOP_WORDS_FILE | uniq -c | sort -rn | cat -n >$OUTPUT_FILE
     ls -l $OUTPUT_FILE
     echo "-------------------------------------"
-    print_preview $OUTPUT_FILE 10
+    print_preview $OUTPUT_FILE $PREVIEW_LENGHT
     ;;
 "C")
+    # Stopwords ignored and will be counted
+    # Results will be presented in a file
+    # Prints PREVIEW_LENGHT to console
     echo "STOPWORDS IGNORED"
     split_words $FILE | tr -d '.,«»;?' | awk NF | sort | uniq -c | sort -rn | cat -n >$OUTPUT_FILE
     ls -l $OUTPUT_FILE
     echo "-------------------------------------"
-    print_preview $OUTPUT_FILE 10
+    print_preview $OUTPUT_FILE $PREVIEW_LENGHT
     ;;
 
 "p")
@@ -207,7 +258,7 @@ case $MODE in
 
 "t")
     echo "STOP WORDS will be filtered out"
-    split_words $FILE | sort | grep -w -v -i -f $STOP_WORD_FILE | uniq -c | sort -rn | cat -n | sed -n 1,"$WORD_STATS_TOP"p >$OUTPUT_FILE
+    split_words $FILE | sort | grep -w -v -i -f $STOP_WORDS_FILE | uniq -c | sort -rn | cat -n | sed -n 1,"$WORD_STATS_TOP"p >$OUTPUT_FILE
     ls -l $OUTPUT_FILE
     echo "-------------------------------------"
     echo "# TOP $WORD_STATS_TOP elements"
@@ -222,17 +273,23 @@ case $MODE in
     echo "# TOP $WORD_STATS_TOP elements"
     print_preview $OUTPUT_FILE $WORD_STATS_TOP
     ;;
+
 esac
+
+# ────────────────────────────────────────────────────────────────────────────────
 
 #
 # ────────────────────────────────────────────────────────────────── CLOSING ─────
 #
+# Here aditional temporary files can be deleted and extra code can
+# be implemented before closing the script
 
-# save files
+# Removing the temporary PDF content file
+rm -f $OUTPUT_TEMP
+
+# Exiting the program
+close
 
 #
 # ───────────────────────────────────────────────────────────────── END CODE ─────
 #
-
-rm -f $OUTPUT_TEMP
-close
