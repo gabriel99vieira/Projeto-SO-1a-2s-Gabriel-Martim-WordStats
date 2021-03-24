872026eaf6786177373164d1cf1c38b9ceb0cb09
Gabriel Vieira <gv1999mpc@hotmail.com>
2021-03-24 23:59:57 +0000

    Corrigidos detalhes na apresentação de mensagens e resultados
    Código completamente comentado

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-sudo apt install poppler-utils
-sudo apt install gnuplot
-sudo apt install libqt5core5a
-sudo apt install binutils
+sudo apt install poppler-utils gnuplot libqt5core5a binutils
--- wordStats.sh
+++ wordStats.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Scripted by
+# Gabriel Vieira - 2200661 - Student at IPL - ESTG - Computer Engineering
+# Martim Silva - 2200661 - Student at IPL - ESTG - Computer Engineering
+# Operative Systems Project
+
+#
+
 #
 # ───────────────────────────────────────────────────────── GLOBAL VARIABLES ─────
 #
@@ -12,28 +19,31 @@ ISO=$3
 # StopWords files
 # Example pt.stop_words.txt
 STOP_WORDS_FILE="stop_words.txt"
-LANG_PATH="./lang"
+LANG_PATH="./StopWords"
 EXTRA_CHARS='.,]:«}#/»=\;"(*<>)|?{•–[-'
 
 # Stopwords related variables
-# WORD_STATS_TOP=10 # !!! Changed to environment cariable (lines +-280)
+# WORD_STATS_TOP=10 # !!! Changed to environment cariable (lines +-425)
 WORD_STATS_TOP_DEFAULT=10
 WORD_STATS_TOP=$(printenv WORD_STATS_TOP)
 
-#GNUPlot
+# EOL LF fix temporary file for storpwords
+WORDS_LF=".temp_sw"
+
+# GNUPlot variables
 GNU_PLOT_TEMP_FILE=".temp_plot"
 GNU_PLOT_OUTPUT=".png"
 GNU_PLOT_OUTPUT_HTML=".html"
 
-# Default preview lenght
+# Default preview lenght when WORD_STATS_TOP isn't used for the required mode
 PREVIEW_LENGHT=10
 
-# Input files
-WORDS_LF=".temp_sw"
+# Input variables
+ORIGINAL_INPUT=$FILE # NEVER! change this variable!
 
-# Output file
-OUTPUT_FILE="results/result---"
-OUTPUT_FILE_FORMAT="txt"
+# Output file variables
+OUTPUT_FILE="results/result---" # ? File prefix - later is changed to full path output
+OUTPUT_FILE_FORMAT="txt"        #
 OUTPUT_TEMP=".temp_out"
 
 # Allowed ISOs
@@ -46,14 +56,76 @@ MODES=("c" "C" "t" "T" "p" "P")
 declare -A FILE_TYPES
 FILE_TYPES["txt"]="Text"
 FILE_TYPES["pdf"]="PDF"
+# ? Example # FILE_TYPES["docx"]="Word document"
 
 #
 # ──────────────────────────────────────────────────────────────── FUNCTIONS ─────
 #
 
+# Actions executed before closing script
+# This will be executed each time function 'close' is called
+before_close() {
+    # Removing the temporary PDF content file
+    rm -f $OUTPUT_TEMP
+    # Removing the temporary EOL LF file
+    rm -f $WORDS_LF
+    # Removing the temporary gnuplot configuration file
+    rm -f $GNU_PLOT_TEMP_FILE
+}
+
+# Returns true if provided string is empty
+# ? USAGE: if string_empty "$string"; then ... fi
+string_empty() {
+    if [ -z "${1}" ]; then
+        return 0
+    fi
+    return 1
+}
+
+# Returns true if both strings are equal/match
+# ! MAX 2 strings
+# ? USAGE: if string_equal "$string1" "$string2"; then ... fi
+string_equal() {
+    if [ "${1}" = "${2}" ]; then
+        return 0
+    fi
+    return 1
+}
+
+# Verifies if input is number
+# WARNING: function input must be string format (for variables)
+# ? USAGE: if is_number $NUMBER; then ... fi
+is_number() {
+    if string_empty "$1"; then
+        log "error" "Incorrect usage of 'is_number'. 1st argument required."
+        close
+    fi
+
+    if [[ $1 =~ ^[+-]?[0-9]+$ ]]; then
+        return 0
+
+    elif [[ $1 =~ ^[+-]?[0-9]+\.?[0-9]*$ ]]; then
+        return 0
+    fi
+    return 1
+}
+
+# Returns true if provided char is a capital letter
+# ? USAGE: if is_capital_letter "$char"; then ... fi
+is_capital_letter() {
+    if string_empty "$1"; then
+        log "error" "Incorrect usage of 'is_capital_letter'. 1st argument required."
+        close
+    fi
+    if [[ $1 =~ [A-Z] ]]; then
+        return 0
+    fi
+    return 1
+}
+
 # Sets a color for the next console output
-# USAGE: color "red|green|blue|yellow|cyan" # to change color
-# USAGE: color # to reset
+# ? USAGE: color "red|green|blue|yellow|cyan" # to change color
+# ? USAGE: color # to reset
 color() {
     case "${1}" in
     "red")
@@ -81,7 +153,8 @@ color() {
 }
 
 # Prints a start log with the specified category name and respective color
-# USAGE: log "info|warn|error"
+# ? USAGE: log "info|warn|error"
+# ? USAGE: log "info|warn|error" "Description"
 log() {
     case "${1}" in
     "error")
@@ -102,83 +175,57 @@ log() {
         ;;
     esac
     color
+
+    if ! string_empty "$2"; then
+        echo "$2"
+    fi
 }
 
 # Exits the current execution with a message
-# USAGE: close
+# ? USAGE: close
 close() {
     echo
     echo "Closing..."
+    before_close
     exit 0
 }
 
 # Verifies if a value exists/is set in the array
-# USAGE: if in_array {parameter} in {array}; then ... fi
+# ? USAGE: if in_array {parameter} in {array}; then ... fi
 in_array() {
     if [ "$2" != in ]; then
-        echo "!!! Incorrect usage of 'in_array'"
-        echo "Correct usage: in_array {key} in {array}"
-        return
+        log "error" "Incorrect usage of 'in_array'. Correct usage: in_array {key} in {array}"
+        close
     fi
     eval '[[ " ${'"$3"'[@]} " =~ " ${'"$1"'} " ]]'
 }
 
 # Verifies if a index exists/is set in the array
-# USAGE: if index_in_array {index} in {array}; then ... fi
+# ? USAGE: if index_in_array {index} in {array}; then ... fi
 index_in_array() {
     if [ "$2" != in ]; then
-        echo "!!! Incorrect usage of 'index_in_array'"
-        echo "Correct usage: in_array {key} in {array}"
+        log "error" "Incorrect usage of 'index_in_array'. Correct usage: in_array {key} in {array}"
         return
     fi
     eval '[ '"$3"'["'"$1"'"] ]'
 }
 
-# Verifies if input is number
-# WARNING: function input must be string format (for variables)
-# USAGE: if is_number $NUMBER; then ... fi
-is_number() {
-    if [ "$1" == "" ]; then
-        log "error"
-        echo "Incorrect usage of 'is_number'. 1st argument required."
-        close
-    fi
-
-    if [[ $1 =~ ^[+-]?[0-9]+$ ]]; then
-        return 0
-
-    elif [[ $1 =~ ^[+-]?[0-9]+\.?[0-9]*$ ]]; then
-        return 0
-    fi
-    return 1
-}
-
-# todo comment
-is_capital() {
-    if [[ $1 =~ [A-Z] ]]; then
-        return 0
-    fi
-    return 1
-}
-
 # Verifies if a file exists
-# USAGE: if file_exists FILE_PATH; then ... fi
+# ? USAGE: if file_exists FILE_PATH; then ... fi
 file_exists() {
-    if [ "$1" == "" ]; then
-        log "error"
-        echo "Incorrect usage of 'file_exists'. 1st argument required."
+    if string_empty "$1"; then
+        log "error" "Incorrect usage of 'file_exists'. 1st argument required."
         close
     fi
     eval '[[ -f "${'"$1"'}" ]]'
 }
 
 # Splits the content of a file in words
-# USAGE: split_words $FILE_PATH
+# ? USAGE: split_words $FILE_PATH
 split_words() {
-    if [ "$1" == "" ]; then
-        echo "!!! Incorrect usage of 'split_words'"
-        echo "A file path must be passed."
-        return
+    if string_empty "$1"; then
+        log "error" "Incorrect usage of 'split_words'. A file path must be passed."
+        close
     fi
     for word in $(cat $1); do
         echo $word
@@ -188,16 +235,14 @@ split_words() {
 # Prints from a specific file the ammount of lines provided
 # USAGE: print_preview $FILE_PATH {int}
 print_preview() {
-    if [ "$1" == "" ]; then
-        echo "!!! Incorrect usage of 'print_preview'."
-        echo "A file path must be passed as 1st parameter."
+    if string_empty "$1"; then
+        log "error" "Incorrect usage of 'print_preview'. A file path must be passed as 1st parameter."
         return
     fi
     re='^[0-9]+$'
     if ! [[ $2 =~ $re ]]; then
-        echo "!!! Incorrect usage of 'print_preview'."
-        echo "A number must be passed as 2nd parameter."
-        return
+        log "error" "Incorrect usage of 'print_preview'. A number must be passed as 2nd parameter."
+        close
     fi
     unset re
     eval 'head -$2 $1'
@@ -207,26 +252,39 @@ print_preview() {
     fi
 }
 
-# todo comment
-# todo refactor
+# Processes the t_mode function into a png plot.
+# If this function does not work it means you have change the code and don't know what you're doing.
+# Variables bellow must be correcly implemented as example
+# GNU_PLOT_OUTPUT=".png"
+# GNU_PLOT_OUTPUT_HTML=".html"
+# MODE=$1   # ? 1st argument on user input when script called
+# ISO=$3    # ? 3rd argument on user input when script called
+# ORIGINAL_INPUT=$FILE
+# OUTPUT_FILE="results/result---"
 plot() {
 
+    # Defines path variables to working files
     GNU_PLOT_OUTPUT=$OUTPUT_FILE$GNU_PLOT_OUTPUT
     GNU_PLOT_OUTPUT_HTML=$OUTPUT_FILE$GNU_PLOT_OUTPUT_HTML
 
+    # if temporary file not exists one is created
     if ! file_exists GNU_PLOT_TEMP_FILE; then
         touch $GNU_PLOT_TEMP_FILE
     fi
-    true >$GNU_PLOT_TEMP_FILE
-    {
-        stpwords=""
-        if is_capital $MODE; then
-            stpwords="No"
-        else
-            stpwords="Yes ($ISO)"
-        fi
 
-        echo "set title \"Top words for $FILE\n$(date +'%A %B %Y %H:%M')\nStopwords: $stpwords\""
+    # Temporary file is emptied
+    true >$GNU_PLOT_TEMP_FILE
+
+    # Processes the gnuplot commands and assigns them to the temporary file
+    stopwords=""
+    if is_capital_letter $MODE; then
+        stopwords="No"
+    else
+        stopwords="Yes ($ISO)"
+    fi
+
+    {
+        echo "set title \"Top words for '$ORIGINAL_INPUT'\n$(date +'%A %B %Y %H:%M')\nStopwords: $stopwords\""
         echo "set terminal png"
         echo "set autoscale y"
         echo "set output \"$GNU_PLOT_OUTPUT\""
@@ -240,35 +298,46 @@ plot() {
         echo "set key top"
         echo "plot \"$OUTPUT_FILE\" using 1:2:xtic(3) with boxes title 'Occurrences' linecolor rgb \"#3399ff\""
     } >"$GNU_PLOT_TEMP_FILE"
-    unset stpwords
+    unset stopwords
 
+    # Creating the plot
     gnuplot <"$GNU_PLOT_TEMP_FILE"
 
+    # Creating the html content for the html file (only presents image)
     {
-        echo "<html><div "
-        echo "style=\"display: block; margin-left: auto; margin-right: auto; width: 40%; margin-top: 5%;"
-        echo "><img src=\"./../$GNU_PLOT_OUTPUT\"></div></html>"
+        echo "<html>
+        <div style=\"display: block; margin-left: auto; margin-right: auto; width: 40%; margin-top: 5%;\">
+        <img src=\"./../$GNU_PLOT_OUTPUT\">
+        </div>
+        </html>"
     } >$GNU_PLOT_OUTPUT_HTML
 
+    # Opens a new window in background with the created plot image
     echo
-    log "info"
-    echo "Opening preview..."
+    log "info" "Opening preview..."
+    # ! 'display' command not used because is not compatible with all SO's
+    # ! Not compatible with WSL
+    # Opened in background to let the script continue it's execution
     xdg-open $GNU_PLOT_OUTPUT >/dev/null 2>&1 &
-
+    # display $GNU_PLOT_OUTPUT &
 }
 
-# todo comment
+# Processes the $FILE and outputs the result to $OUTPUT_FILE
+# Uses the variables bellow as example
+# STOP_WORDS_FILE="stop_words.txt"
+# WORD_STATS_TOP=$(printenv WORD_STATS_TOP)
+# FILE=$2   # ? 2rd argument on user input when script called
+# EXTRA_CHARS='.,]:«}#/»=\;"(*<>)|?{•–[-'
+# OUTPUT_FILE="results/result---"
 c_mode() {
     # Checks what mode the user entered
     cmd=""
     if [ "$MODE" == "c" ]; then
-        log "exec"
-        echo "STOPWORDS FILTERED"
+        log "exec" "STOPWORDS FILTERED"
         # Saves command to filter the stopwords
         cmd="sort | grep -w -v -i -f $STOP_WORDS_FILE"
     elif [ "$MODE" == "C" ]; then
-        log "exec"
-        echo "STOPWORDS IGNORED"
+        log "exec" "STOPWORDS IGNORED"
         # Saves command without the grep to ignore Stopwords
         cmd="sort"
     fi
@@ -277,40 +346,58 @@ c_mode() {
     # Prints PREVIEW_LENGHT to console
     split_words $FILE | tr -d "'" | tr -d "$EXTRA_CHARS" | awk NF | eval $cmd | uniq -c -i | sort -rn | cat -n | tr -d '\t' >$OUTPUT_FILE
     unset cmd
-    ls -lah $OUTPUT_FILE
+
+    echo
     echo "-------------------------------------"
     print_preview $OUTPUT_FILE $PREVIEW_LENGHT
+    echo "-------------------------------------"
+    echo
+    log "exec" "RESULTS: $OUTPUT_FILE"
+    log "exec"
+    ls -lah $OUTPUT_FILE
+    log "exec" "Distinct words: $(wc -l <$OUTPUT_FILE)"
 }
 
-# todo comment
+# Processes the $FILE and outputs the result to $OUTPUT_FILE
+# Top mode with WORD_STATS_TOP defined either environmentally or in script through WORD_STATS_TOP_DEFAULT
+# Same variables as 'c_mode' used
 t_mode() {
     # Checks what mode the user entered
     cmd=""
     if [ "$MODE" == "t" ]; then
         # Saves command to filter the stopwords
         cmd="sort | grep -w -v -i -f $STOP_WORDS_FILE"
-        log "exec"
-        echo "STOPWORDS FILTERED"
+        log "exec" "STOPWORDS FILTERED"
     elif [ "$MODE" == "T" ]; then
         # Saves command without the grep to ignore Stopwords
         cmd="sort"
-        log "exec"
-        echo "STOPWORDS IGNORED"
-        log "exec"
-        echo "WORD_STATS_TOP =" $WORD_STATS_TOP
+        log "exec" "STOPWORDS IGNORED"
+        log "exec" "WORD_STATS_TOP =" $WORD_STATS_TOP
     fi
 
     # Results will be presented in a file
     # Prints PREVIEW_LENGHT to console
-    split_words $FILE | tr -d "'" | tr -d "$EXTRA_CHARS" | awk NF | eval $cmd | uniq -c -i | sort -rn | cat -n | tr -d '\t' | sed -n 1,"$WORD_STATS_TOP"p >$OUTPUT_FILE
-    unset cmd
-    ls -lah $OUTPUT_FILE
+    tmp_var=$(split_words $FILE | tr -d "'" | tr -d "$EXTRA_CHARS" | awk NF | eval $cmd | uniq -c -i | sort -rn | cat -n | tr -d '\t')
+    dwords=$(wc -l <<<$tmp_var)
+    sed -n 1,"$WORD_STATS_TOP"p <<<$tmp_var >$OUTPUT_FILE
+
+    echo
     echo "-------------------------------------"
-    echo " # TOP $WORD_STATS_TOP elements"
+    printf "\tTOP $WORD_STATS_TOP elements\n"
     print_preview $OUTPUT_FILE $WORD_STATS_TOP
+    echo "-------------------------------------"
+    echo
+    log "exec" "RESULTS: $OUTPUT_FILE"
+    log "exec"
+    ls -lah $OUTPUT_FILE
+    log "exec" "Distinct words: $dwords"
+
+    unset cmd
+    unset tmp_var
+    unset dwords
 }
 
-# todo comment
+# Uses the 't_mode' and then processes the plot using the 'plot' function
 p_mode() {
     if [ "$MODE" == "p" ]; then
         MODE="t"
@@ -335,15 +422,12 @@ echo
 
 # Check if mode is allowed
 if in_array MODE in MODES; then
-    log "info"
-    echo "Executing on mode '$MODE'."
+    log "info" "Executing on mode '$MODE'."
 else
-    if [ -n "${MODE}" ]; then
-        log "error"
-        echo "Mode required do execute [C/c|P/p|T/t]"
+    if string_empty $MODE; then
+        log "error" "Mode required do execute [C/c|P/p|T/t]"
     else
-        log "error"
-        echo "unknown command '$MODE'"
+        log "error" "Unknown mode '$MODE'"
     fi
     close
 fi
@@ -356,11 +440,9 @@ if file_exists FILE; then
 
     # check if file type is allowed
     if index_in_array extension in FILE_TYPES; then
-        log "info"
-        echo "Opening '$FILE' as ${FILE_TYPES[${extension}]} file."
+        log "info" "Opening '$FILE' as ${FILE_TYPES[${extension}]} file."
     else
-        log "error"
-        echo "File extension '$extension' not allowed."
+        log "error" "File extension '$extension' not allowed."
         close
     fi
 else
@@ -375,12 +457,10 @@ fi
 
 # Check if ISO is suported, else use default "en"
 if in_array ISO in ISOS; then
-    log "info"
-    echo "ISO format: '$ISO'."
+    log "info" "ISO format: '$ISO'."
 else
     ISO="en"
-    log "warn"
-    echo "ISO not defined. Default will be used ('$ISO')."
+    log "warn" "ISO not defined. Default will be used ('$ISO')."
 fi
 
 #
@@ -392,18 +472,15 @@ fi
 # Evaluates if Environment variable WORD_STATS_TOP is assigned
 #       If assigned then proceeds the normal execution
 #       Else warn pops up in console and a default is assigned by WORD_STATS_TOP_DEFAULT variable
-if [ "${WORD_STATS_TOP}" == "" ]; then
+if string_empty "$WORD_STATS_TOP"; then
     export WORD_STATS_TOP=$((WORD_STATS_TOP_DEFAULT))
-    log "warn"
-    echo "WORD_STATS_TOP not defined. Default used ($WORD_STATS_TOP_DEFAULT)"
+    log "warn" "WORD_STATS_TOP: undefined (default :$WORD_STATS_TOP_DEFAULT)"
 else
     if is_number $WORD_STATS_TOP; then
-        log "info"
-        echo "WORD_STATS_TOP: $WORD_STATS_TOP"
+        log "info" "WORD_STATS_TOP: $WORD_STATS_TOP"
     else
         export WORD_STATS_TOP=$((WORD_STATS_TOP_DEFAULT))
-        log "warn"
-        echo "WORD_STATS_TOP not a number. Default used ($WORD_STATS_TOP_DEFAULT)"
+        log "warn" "WORD_STATS_TOP not a number. Default used ($WORD_STATS_TOP_DEFAULT)"
     fi
 fi
 
@@ -411,13 +488,11 @@ fi
 # In case it doesn't exist, one is created empty and a warning pops up
 STOP_WORDS_FILE="$LANG_PATH/$ISO.$STOP_WORDS_FILE"
 if file_exists STOP_WORDS_FILE; then
-    log "info"
-    echo "Stop words file: $STOP_WORDS_FILE. ($(wc -l <$STOP_WORDS_FILE))"
+    log "info" "Stop words file: $STOP_WORDS_FILE. ($(wc -l <$STOP_WORDS_FILE))"
 else
     # Creates the ISO file
     touch $STOP_WORDS_FILE
-    log "warn"
-    echo "Stop words file not found ... creating empty"
+    log "warn" "Stop words file not found ... creating empty"
 fi
 
 # Sets the 'filename' variable the same as input
@@ -431,13 +506,11 @@ OUTPUT_FILE="$OUTPUT_FILE$filename.$OUTPUT_FILE_FORMAT"
 # In case it exists warns that it will be overwritten
 # If it doesn't exist, one will be created
 if file_exists OUTPUT_FILE; then
-    log "warn"
-    echo "Output file will be overwritten: '$OUTPUT_FILE'"
+    log "warn" "Output file will be overwritten: '$OUTPUT_FILE'"
     true >"$OUTPUT_FILE"
     touch "$OUTPUT_FILE"
 else
-    log "info"
-    echo "Creating new output file: '$OUTPUT_FILE'"
+    log "info" "Creating new output file: '$OUTPUT_FILE'"
     touch "$OUTPUT_FILE"
 fi
 
@@ -448,9 +521,9 @@ fi
 # If this step is well written, then the following code will work without any change
 
 # Converting PDF contents into Text
-if [ "${extension}" == "pdf" ]; then
+if string_equal "$extension" "pdf"; then
     touch $OUTPUT_TEMP
-    pdftotext -layout $FILE $OUTPUT_TEMP
+    pdftotext $FILE $OUTPUT_TEMP
     FILE=$OUTPUT_TEMP
 fi
 
@@ -473,18 +546,22 @@ unset extension
 #
 
 echo
-echo "Executing..."
-echo
+log "exec" "Processing '$FILE'"
+
+# Simple case for each one of the modes
 case $MODE in
 
 "c" | "C")
+    log "exec" "COUNT MODE"
     c_mode
     ;;
 
 "p" | "P")
+    log "exec" "PLOT MODE"
     p_mode
     ;;
 "t" | "T")
+    log "exec" "TOP MODE"
     t_mode
     ;;
 
@@ -500,10 +577,8 @@ esac
 # Here aditional temporary files can be deleted and extra code can
 # be implemented before closing the script
 
-# Removing the temporary PDF content file
-rm -f $OUTPUT_TEMP
-rm -f $WORDS_LF
-rm -f $GNU_PLOT_TEMP_FILE
+# * Moved to function 'before_close'
+# But use here your code whatever you like ;)
 
 # Exiting the program
 close
